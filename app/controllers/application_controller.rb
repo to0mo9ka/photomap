3c9a32d09a6408bf597f79f52c214b8822d756b3
EC2 Default User <ec2-user@ip-172-31-28-54.ap-northeast-1.compute.internal>
2023-08-20 10:52:36 +0000

    link_toでページを繋げた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     maps_path # ログイン後に開くページを指定
   end
+  
+   def after_sign_out_path_for(resource)
+    root_path #ログアウト後の遷移先を指定
+   end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
