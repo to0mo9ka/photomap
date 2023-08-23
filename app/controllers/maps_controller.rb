5f33d579c45b91f62bbe7f4b5d57b4093da42c2d
EC2 Default User <ec2-user@ip-172-31-28-54.ap-northeast-1.compute.internal>
2023-08-23 14:54:15 +0000

    account_typeを設定したprivate_accountは見れないようにした

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -15,7 +15,15 @@ class MapsController < ApplicationController
   end
   
   def index
-    @places = Place.where(user_id: [current_user.id, 0])
+    my_post_ids = current_user.places.pluck(:id)
+
+    # 公開アカウントのユーザーIDを取得
+    public_user_ids = User.where(account_type: 'public_account').pluck(:id)
+    
+    post_ids_to_show = my_post_ids + Place.where(user_id: public_user_ids).pluck(:id)
+
+    # 上記の投稿IDを持つ投稿を取得
+    @places = Place.where(id: post_ids_to_show).page(params[:page]).reverse_order
   end
   
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,21 @@
 class User < ApplicationRecord
-  enum account_type: {
-    public_account: 0,
-    private_account: 1
-  }
+  #enum account_type: {
+    #public_account: 0,
+    #private_account: 1
+  #}
   
   #after_initialize :set_default_account_type, if: :new_record?
   #def set_default_account_type
     #self.account_type ||= :public_account
   #end
   
-  def self.account_types
-    {
-      public_account: '公開アカウント',
-      private_account: '非公開アカウント'
+  #def self.account_types
+    #{
+      #public_account: '公開アカウント',
+      #private_account: '非公開アカウント'
       # もしくは必要な他のアカウントタイプを追加
-    }
-  end
+    #}
+  #end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
